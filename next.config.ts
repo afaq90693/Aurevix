5dae4c13fad004c0c503d79cb53bd7992a6cc9e2
afaq90693 <127093873+afaq90693@users.noreply.github.com>
2026-08-19 20:12:58 +0500

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    localPatterns: [
+      { pathname: '/images/**' },
+    ],
+  },
+};
+
+export default nextConfig;
